acce983718096f1a4e60dae2bc2ec7a931ff2db0
SelimBamri <selim.bamri@ihec.u-carthage.tn>
2024-09-09 19:26:16 +0200

    adding chat hub

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly IDictionary<string, UserConnection> _connection;
+
+        public ChatHub(IDictionary<string, UserConnection> connection)
+        {
+            this._connection = connection;
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
+            _connection[Context.ConnectionId] = userConnection;
+            await Clients.Group(userConnection.Room!)
+                .SendAsync("RecieveMessage", "Let's program Bot", $"{userConnection.User} has joined the Group.");
+            await SendConnectedUsers(userConnection.Room!);
+
+        }
+        public async Task SendMessage(string message)
+        {
+            if(_connection.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room!)
+                    .SendAsync("RecieveMessage", userConnection.User, message, DateTime.Now); 
+            }
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if(!_connection.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                return base.OnDisconnectedAsync(exception);
+            }
+            Clients.Group(userConnection.Room!)
+                .SendAsync("RecieveMessage", "Let's program Bot", $"{userConnection.User} has left the Group.");
+            SendConnectedUsers(userConnection.Room!);
+            return base.OnDisconnectedAsync(exception);
+        }   
+
+        public Task SendConnectedUsers(string room)
+        {
+            var users = _connection.Values
+                .Where(x => x.Room == room)
+                .Select(x => x.User);
+            return Clients.Group(room).SendAsync("ConnectedUser", users);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
+using ChatApp.Hubs;
+using ChatApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(opt => new Dictionary<string, UserConnection>());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.MapHub<ChatHub>("/chat");
 
 app.MapControllers();
 
